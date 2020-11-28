d82c37c84873a5ed6c57469104d25424cd4f257a
Fabio Lorenzi <73660100+fabiolorenzi@users.noreply.github.com>
2020-11-28 13:03:06 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TodoList from "./components/TodoList.jsx";
+
+function App() {
+    return (
+        <div id="todo-app">
+            <TodoList />
+        </div>
+    );
+};
+
+export default App;
